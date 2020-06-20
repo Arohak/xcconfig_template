b6fc451da93b7afd6a2837d36092771a23dfd32d
Ara <ara.hakobyan@marktguru.com>
2020-06-20 09:16:40 +0400

    Testing Remote Push Notification in simulator

--- xcconfig_template/AppDelegate.swift
+++ xcconfig_template/AppDelegate.swift
@@ -57,13 +57,63 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        UserDefaultConfig.test()
 //        AtomicConfig.test()
 
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window!.rootViewController = vc
-        self.window!.makeKeyAndVisible()
-
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.rootViewController = vc
+        window!.makeKeyAndVisible()
+        
+        registerForPushNotifications()
+        
         print(Configuration.apiKey)
-
+        
         return true
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate -
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { String(format: "%02.2hhx", $0) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notifications with error: \(error)")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+//        {
+//            "aps" : {
+//                "alert" : {
+//                    "title" : "arohak.com",
+//                    "body" : "A weekly blog about iOS development"
+//                },
+//                "badge" : 5
+//            },
+//            "Simulator Target Bundle": "com.arohak.xcconfig-template_dev"
+//        }
+        
+//        xcrun simctl list
+//        xcrun simctl push <device> <bundle-identifier> <path-to-apns-file>
+//        xcrun simctl push booted payload.apns
+//        xcrun simctl push 40A14EA5-ECEC-4589-A8FA-E98C2679579F payload.apns
+        
+        print("userInfo: \(response.notification.request.content.userInfo)")
+        completionHandler()
+    }
+    
+    private func registerForPushNotifications() {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+            (granted, error) in
+            print("Permission granted: \(granted)")
+            // 1. Check if permission granted
+            guard granted else { return }
+            // 2. Attempt registration for remote notifications on the main thread
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
     }
 }
 
