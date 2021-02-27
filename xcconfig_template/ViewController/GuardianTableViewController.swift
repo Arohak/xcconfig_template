a3c48f147879c06a82054353c60deb6d918e5048
Ara Hakobyan <aro.hak.25@gmail.com>
2021-02-27 15:19:11 +0400

    testing new api provider to fetch data

--- xcconfig_template/ViewController/GuardianTableViewController.swift
+++ xcconfig_template/ViewController/GuardianTableViewController.swift
@@ -9,19 +9,76 @@
 import UIKit
 import AHProvider
 
-public class GuardianTableViewController: UITableViewController {
+public class GuardianTableViewController: UIViewController {
     private let provider = AHProvider<Guardian>()
-    private var feed: NewsFeed? {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+    
+    private let apiProvider = APIProvider()
+    
+    private let oneView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    private let twoView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+
+//    private var feed: NewsFeed? {
+//        didSet {
+//            tableView.reloadData()
+//        }
+//    }
 
     override public func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
 
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        searchGuardianFeed()
+        view.addSubview(oneView)
+//        oneView.anchors(top: (anchor: view.topAnchor, constant: 20),
+//                        left: (anchor: view.leftAnchor, constant: 20),
+//                        right: (anchor: view.rightAnchor, constant: 20),
+//                        bottom: (anchor: view.bottomAnchor, constant: 400))
+        
+        view.addSubview(twoView)
+//        twoView.anchors(top: (anchor: view.topAnchor, constant: 40),
+//                        left: (anchor: view.leftAnchor, constant: 40),
+//                        right: (anchor: view.rightAnchor, constant: 40),
+//                        bottom: (anchor: view.bottomAnchor, constant: 40))
+        
+        twoView.anchors(top: .init(anchor: oneView.bottomAnchor, constant: 40),
+                        centerX: .init(anchor: oneView.centerXAnchor),
+                        height: 200, width: 200)
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//        searchGuardianFeed()
+        getData()
+    }
+    
+    func getData() {
+        let url: URL = "https://content.guardianapis.com/search?page-size=1"
+        let method = "GET"
+        let headers = ["api-key": "test"]
+        
+        apiProvider.fetch(with: url, method: method, headers: headers) { result in
+            switch result {
+            case .success(let data):
+                print("data: ", data)
+            case .failure(let error):
+                print("error: ", error)
+            }
+        }
+        
+//        apiProvider.fetch(with: url, method: method, headers: headers) { (response: Result<NewsFeed, AHError>) in
+//            switch response {
+//            case .failure(let error):
+//                print(error)
+//            case .success(let result):
+//                print(result)
+//            }
+//        }
     }
 
     func searchGuardianFeed() {
@@ -39,19 +96,19 @@ public class GuardianTableViewController: UITableViewController {
 //        }
     }
 
-    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return feed?.response.results.count ?? 0
-    }
-
-    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//        if cell == nil {
-//            cell = UITableViewCell(style: .value2, reuseIdentifier: "cellIdentifier")
-//        }
-//        let item = feed?.response.results[indexPath.row]
-        cell.textLabel?.text = "1"//item?.pillarName
-//        cell.detailTextLabel?.text = "22222" //item?.sectionName
-        return cell
-    }
+//    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return feed?.response.results.count ?? 0
+//    }
+//
+//    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+////        if cell == nil {
+////            cell = UITableViewCell(style: .value2, reuseIdentifier: "cellIdentifier")
+////        }
+////        let item = feed?.response.results[indexPath.row]
+//        cell.textLabel?.text = "1"//item?.pillarName
+////        cell.detailTextLabel?.text = "22222" //item?.sectionName
+//        return cell
+//    }
 }
 
